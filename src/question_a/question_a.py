339d215b11467cddcc902411b7bf7aaa756937c9
Ana Herrera Flores <ana.herreraflores@g.austincc.edu>
2025-10-16 01:17:04 +0000

    question 1 done

--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,11 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+
+def get_sum_of_evens(num):
+    """
+    Returns the sum of all even numbers from 2 up to and including num.
+    Example: get_sum_of_evens(10) returns 30 (2+4+6+8+10)
+    """
+    return sum(i for i in range(2, num + 1, 2))
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -2,11 +2,22 @@
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
+from src.question_a.question_a import test_config, get_sum_of_evens
 
 class Test_Config(unittest.TestCase):
 
     def test_question_a_config(self):
         self.assertEqual(True, test_config())
 
+
+class Test_Question_A(unittest.TestCase):
+
+    def test_get_sum_of_evens_11(self):
+        self.assertEqual(30, get_sum_of_evens(11))
+    
+    def test_get_sum_of_evens_10(self):
+        self.assertEqual(30, get_sum_of_evens(10))
+    
+    def test_get_sum_of_evens_8(self):
+        self.assertEqual(20, get_sum_of_evens(8))
 
